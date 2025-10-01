38c7de7c76dd916124fbf98e6fdade2561de2208
oliyka <olja.x.06@gmail.com>
2025-10-01 22:43:30 +0300

    added routes and controllers

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -6,7 +6,7 @@ export const getNotes = async (req, res) => {
   res.status(200).json(notes);
 };
 
-export const getNOteById = async (req, res, next) => {
+export const getNoteById = async (req, res, next) => {
   const { noteId } = req.params;
   const note = await Note.findById(noteId);
 
@@ -14,5 +14,41 @@ export const getNOteById = async (req, res, next) => {
     next(createHttpError(404, 'Note not found'));
     return;
   }
+  res.status(200).json(note);
+};
+
+export const createNote = async (req, res) => {
+  const note = await Note.create(req.body);
+  res.status(200).json(note);
+};
+
+export const deleteNote = async (req, res, next) => {
+  const { noteId } = req.params;
+  const note = await Note.findOneAndDelete({
+    _id: noteId,
+  });
+
+  if (!note) {
+    next(createHttpError(404, "Notet not found"));
+    return;
+  }
+
+  res.status(200).send(note);
+};
+
+
+export const updateNote = async (req, res, next) => {
+  const { noteId } = req.params;
+  const note = await Note.findOneAndUpdate(
+    { _id: noteId },
+    req.body,
+    {new: true},
+  );
+
+  if (!note) {
+    next(createHttpError(404, 'Note not found'));
+    return;
+  }
+
   res.status(200).json(note);
 };
--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -1,9 +1,12 @@
 import { Router } from 'express';
-import { getNotes, getNOteById } from '../controllers/notesController.js';
+import { getNotes, getNoteById, createNote, deleteNote, updateNote } from '../controllers/notesController.js';
 
 const router = Router();
 
 router.get('/notes', getNotes);
-router.get('/notes/:noteId', getNOteById);
+router.get('/notes/:noteId', getNoteById);
+router.post('/notes', createNote);
+router.delete('/notes/:noteId', deleteNote);
+router.patch('/notes/:noteId', updateNote);
 
 export default router;
