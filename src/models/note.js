a1395e8fcb192067293d2aba93dba5231131c5ed
oliyka <olja.x.06@gmail.com>
2025-10-09 21:52:11 +0300

    fix

--- src/models/note.js
+++ src/models/note.js
@@ -1,22 +1,25 @@
 
 import { Schema } from 'mongoose';
 import { model } from 'mongoose';
+import TAGS from '../constants/tags.js';
 
 const noteSchema = new Schema(
   {
     title: {
       type: String,
       required: true,
+      trim: true,
     },
     content: {
       type: String,
       required: false,
       default: "",
+      trim: true,
     },
     tag: {
       type: String,
       required: false,
-      enum: ["Work", "Personal", "Meeting", "Shopping", "Ideas", "Travel", "Finance", "Health", "Important", "Todo"],
+      enum: TAGS,
       default: "Todo",
     },
   },
