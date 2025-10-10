7553ddb7ca64277679122cad289aeb44a69f2e60
oliyka <olja.x.06@gmail.com>
2025-10-10 19:16:39 +0300

    delete excessive

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -25,7 +25,7 @@ export const createNoteSchema = {
   [Segments.BODY]: Joi.object({
     title: Joi.string().min(1).required(),
     content: Joi.string().allow(""),
-    tag: Joi.string().valid(...TAGS).required(),
+    tag: Joi.string().valid(...TAGS),
   })
 };
 
