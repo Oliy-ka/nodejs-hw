8f2124ff6f53831c46776031edcba3406a3281ce
oliyka <olja.x.06@gmail.com>
2025-10-22 23:37:01 +0300

    added img

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import { errors } from "celebrate";
 import authRoutes from './routes/authRoutes.js';
 import cookieParser from "cookie-parser";
@@ -20,6 +21,7 @@ app.use(cookieParser());
 
 app.use(authRoutes);
 app.use(notesRoutes);
+app.use(userRoutes);
 
 app.use(notFoundHandler);
 
